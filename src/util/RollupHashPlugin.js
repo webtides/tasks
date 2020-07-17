c9f4afe4b3223a44af378ad8187cd827d02bc4d1
Eddy Löwen <mail@eddyloewen.com>
2020-07-17 11:32:13 +0200

    fixes missing “/“ in filepath for js files from rollup-hash-plugin

--- src/util/RollupHashPlugin.js
+++ src/util/RollupHashPlugin.js
@@ -12,8 +12,8 @@ export default function hash(options = {}) {
 
 			// if outputOptions.entryFileNames contains [hash] we need to extract it from the path for the key in the manifest
 			const fileExtension = chunk.fileName.split('.').pop();
-			const keyFileName = `${filePath}${chunk.name}.${fileExtension}`;
-			const valueFileName = `${filePath}${chunk.fileName}`;
+			const keyFileName = `${filePath}/${chunk.name}.${fileExtension}`;
+			const valueFileName = `${filePath}/${chunk.fileName}`;
 
 			if (chunk.isEntry && !valueFileName.includes('.map')) {
 				addToManifest(keyFileName, valueFileName, code, options);
