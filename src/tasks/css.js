c6bd450e064cb9e8f8b18fdb4201a37419cef693
Eddy Löwen <mail@eddyloewen.com>
2020-07-17 11:01:09 +0200

    adds gulp-flatten to css task
    and makes it configurable to enable or disable the flatten feature

--- src/tasks/css.js
+++ src/tasks/css.js
@@ -1,4 +1,5 @@
 import gulp from 'gulp';
+import flatten from 'gulp-flatten';
 import plumber from 'gulp-plumber';
 import postcss from 'gulp-postcss';
 import sourcemaps from 'gulp-sourcemaps';
@@ -9,6 +10,10 @@ import hash from 'src/util/GulpHashPlugin';
 import Config from 'src/config.js';
 
 export default (options = {}) => {
+	options = {
+		flatten: true,
+		...options,
+	};
 	return () => {
 		return gulp
 			.src(options.src)
@@ -28,6 +33,7 @@ export default (options = {}) => {
 			.pipe(sourcemaps.init())
 			.pipe(postcss())
 			.pipe(sourcemaps.write('.'))
+			.pipe(gulpIf(options.flatten, flatten()))
 			.pipe(gulp.dest(options.dest))
 			.pipe(gulpIf(Config.versionManifest !== false, hash(Config.versionManifest)));
 	};
