6050fa3468753ea8a93f5ef669424caeb16936c5
Eddy Löwen <mail@eddyloewen.com>
2020-08-05 11:34:09 +0200

    adds config option to svg task

--- src/tasks/svg.js
+++ src/tasks/svg.js
@@ -2,17 +2,19 @@ import gulp from 'gulp';
 import svgSprite from 'gulp-svg-sprite';
 import merge from 'merge-stream';
 
-const svgTask = (src, dest) => {
+const defaultConfig = { mode: { symbol: true } };
+
+const svgTask = (src, dest, config) => {
 	return gulp
 		.src(src)
-		.pipe(svgSprite({ mode: { symbol: true } }))
+		.pipe(svgSprite(config || defaultConfig))
 		.pipe(gulp.dest(dest));
 };
 
 export default (options) => {
 	return () => {
 		const streams = options.paths.map((path) => {
-			return svgTask(path.src, path.dest);
+			return svgTask(path.src, path.dest, path.config || defaultConfig);
 		});
 
 		return merge(streams);
