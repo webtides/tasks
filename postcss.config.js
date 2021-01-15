9bf308fcafc16b2dd389b30fea8eb248d6bc9eb2
Eddy Löwen <mail@eddyloewen.com>
2021-01-15 11:32:07 +0100

    updates everything for postcss 8
    - This is so that we can start using tailwindcss 2.0 in projects
    - cssnano is not currently updated for postcss 8 - but since postcss 8.2 the old API should still work

--- postcss.config.js
+++ postcss.config.js
@@ -1,7 +1,6 @@
 module.exports = ({ env }) => ({
 	plugins: [
 		require('postcss-import'),
-		require('tailwindcss'),
 		require('postcss-nested'),
 		require('autoprefixer'),
 		env === 'production'
