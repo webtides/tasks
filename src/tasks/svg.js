381cf62183fda176a684fe0d4db65b658bdbc7e3
Markus Hesper <mh@19f.de>
2024-06-11 19:10:34 +0200

    feat(svg): adds a vanilla svg-sprite implementation

--- src/tasks/svg.js
+++ src/tasks/svg.js
@@ -1,22 +1,54 @@
-import gulp from 'gulp';
-import svgSprite from 'gulp-svg-sprite';
-import merge from 'merge-stream';
+import glob from 'glob';
+import SVGSprite from 'svg-sprite';
+import File from 'vinyl';
+import path from 'path';
+import fs from 'fs';
 
-const defaultConfig = { mode: { symbol: true } };
+const defaultConfig = { mode: { symbol: true }, dest: '.' };
 
-const svgTask = (src, dest, config) => {
-	return gulp
-		.src(src)
-		.pipe(svgSprite(config || defaultConfig))
-		.pipe(gulp.dest(dest));
+const compileSvgSprite = (src, config = defaultConfig) => {
+	const spriter = new SVGSprite(config);
+	const cwd = process.cwd();
+	// Find SVG files recursively via `glob`
+	const files = glob.sync(src, { cwd });
+	files.forEach((file) => {
+		try {
+			spriter.add(
+				new File({
+					path: path.join(cwd, file), // Absolute path to the SVG file
+					base: cwd, // Base path (see `name` argument)
+					contents: fs.readFileSync(path.join(cwd, file)), // SVG file contents
+				}),
+			);
+		} catch (e) {
+			console.error(e);
+		}
+	});
+
+	// Compile the sprite
+	spriter.compile((error, result) => {
+		/* Write `result` files to disk (or do whatever with them ...) */
+		for (const mode of Object.values(result)) {
+			for (const resource of Object.values(mode)) {
+				fs.mkdirSync(path.dirname(resource.path), { recursive: true });
+				fs.writeFileSync(resource.path, resource.contents);
+			}
+		}
+	});
 };
 
-export default (options) => {
-	return () => {
-		const streams = options.paths.map((path) => {
-			return svgTask(path.src, path.dest, path.config || defaultConfig);
+export const svg = (options) => {
+	return (done) => {
+		options.paths.forEach(async (path) => {
+			const { src, config } = path;
+			if (!src) {
+				throw new Error(`${JSON.stringify(path)} path must have "src" property`);
+			}
+			await compileSvgSprite(src, config);
 		});
-
-		return merge(streams);
+		done();
+		return true;
 	};
 };
+
+export default svg;
