1ae00e5c777435e8d58b8546bd9babc0c39cf0c8
Markus Hesper <mh@19f.de>
2024-06-12 11:41:01 +0200

    feat(copy): fin

--- src/tasks/copy.js
+++ src/tasks/copy.js
@@ -3,9 +3,8 @@ import fs from 'fs-extra';
 import path from 'path';
 import log from 'fancy-log';
 
-const copyFiles = async (pattern, dest) => {
-	// TODO pass globConfig?
-	const files = await glob(pattern, { nodir: true });
+const copyFiles = async (pattern, dest, flat = true) => {
+	const files = await glob(pattern, { nodir: flat });
 	files.forEach((file) => {
 		const destPath = path.join(dest, path.basename(file));
 		fs.copy(file, destPath, (err) => {
@@ -21,11 +20,11 @@ const copyFiles = async (pattern, dest) => {
 export default (options) => {
 	return (done) => {
 		options.paths.forEach(async (path) => {
-			const { src, dest } = path;
+			const { src, dest, flat } = path;
 			if (!src || !dest) {
 				throw new Error(`${JSON.stringify(path)} path must have "src" and "dest" properties`);
 			}
-			await copyFiles(src, dest);
+			await copyFiles(src, dest, flat);
 		});
 		done();
 		return true;
