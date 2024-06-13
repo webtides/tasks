438b9b3988c2050cf58daafd88867ed7e10305db
Markus Hesper <mh@19f.de>
2024-06-13 13:36:05 +0200

    feat(hash): change rollup plugin to write in generate bundle hook as hashes were not in place in renderChunk

--- src/util/RollupHashPlugin.js
+++ src/util/RollupHashPlugin.js
@@ -7,17 +7,17 @@ export default function hash(options = {}) {
 
 	return {
 		name: 'hash-version-manifest',
-		renderChunk: function (code, chunk, outputOptions) {
+		generateBundle: function (outputOptions, bundle) {
 			const filePath = options.formatter(outputOptions.dir.split(path.sep).join('/'));
+			Object.entries(bundle).forEach(([key, chunk]) => {
+				const fileExtension = chunk.fileName.split('.').pop();
+				const keyFileName = `${filePath}/${chunk.name}.${fileExtension}`;
+				const valueFileName = `${filePath}/${chunk.fileName}`;
 
-			// if outputOptions.entryFileNames contains [hash] we need to extract it from the path for the key in the manifest
-			const fileExtension = chunk.fileName.split('.').pop();
-			const keyFileName = `${filePath}/${chunk.name}.${fileExtension}`;
-			const valueFileName = `${filePath}/${chunk.fileName}`;
-
-			if (chunk.isEntry && !valueFileName.includes('.map')) {
-				addToManifest(keyFileName, valueFileName, code, options);
-			}
+				if (chunk.isEntry && !valueFileName.includes('.map')) {
+					addToManifest(keyFileName, valueFileName, chunk.code, options);
+				}
+			});
 		},
 	};
 }
